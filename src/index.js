d9972cb600059b668499b8ff6d3369d5fa165529
Sergey Slastnikov <codemonkey2263@gmail.com>
2022-02-23 23:50:12 +0400

    init: morse decoder function

--- src/index.js
+++ src/index.js
@@ -35,10 +35,47 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********' : ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+  array = [];
+  for (var i = 0; i < expr.length; i+=10) {
+    array.push(expr.slice(i, i + 10));
+  }
+  let noZeroArray = [];
+  array.forEach(function(item) {
+    if (item.indexOf('1') !== -1) {
+    item = item.slice(item.indexOf('1'));
+      }
+    noZeroArray.push(item); 
+  })
+
+   let morseArray = [];
+  noZeroArray.forEach(function(item){
+    let tempArray = [];
+    let tempString = '';
+      for (var i = 0; i < item.length; i+=2) {
+    tempArray.push(item.slice(i, i + 2)); 
+  }
+    tempArray.forEach(function(item) {
+      (item === '10' ? tempString += '.' : 0);
+      (item === '11' ? tempString += '-' : 0);
+      (item === '**' ? tempString += '**' : 0);
+    })
+    morseArray.push(tempString);
+  })
+  
+  let result = '';
+  let decodingArray = Object.entries(MORSE_TABLE);
+  
+  morseArray.forEach(function(item) {
+    for(i = 0; i < decodingArray.length; i++) 
+     if(item === decodingArray[i][0] ){
+        result += decodingArray[i][1];
+      }
+  })
+  return result;
 }
 
 module.exports = {
